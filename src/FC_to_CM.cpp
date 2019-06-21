0b20e8f4c15c41614f670a97b37c6d111db829f4
crussell2021 <50681298+crussell2021@users.noreply.github.com>
2019-06-21 14:05:35 -0700

    implemented listen for rx and responces

--- src/FC_to_CM.cpp
+++ src/FC_to_CM.cpp
@@ -1,7 +1,11 @@
-#include "FC_to_CM.h
+#include "FC_to_CM.h"
 
-FC_to_CM::FC_to_CM() {
-    
+FC_to_CM::FC_to_CM(PinName tx, PinName rx) : _xbee(tx, rx) {
+  _timeout = 100;
+  _cmAddress = 0x0013A2004192DBBD;
+  _rx_thread.start(callback(this, &FC_to_CM::_listen_for_rx));
+  flightState = 0x10;
+  rsvpState = 0x00;
 }
 
 void FC_to_CM::_listen_for_rx() {
@@ -15,14 +19,26 @@ void FC_to_CM::_listen_for_rx() {
       len = _xbee.rxPacket(msg, &sender);
       if (len > 0) {
         switch (msg[0]) {
-          case 0x10:  // Response to invite
-            _process_rsvp(sender, msg[1]);
+          case 0x00:  // Invitation to connect
+            _process_invitation();    //send 0x10 responce
             break;
-          case 0x31:  // Response to clock test
-            _process_clock_test(sender, msg[1]);
+          case 0x01:  // primed for launch
+            flightState = 0x00;   
             break;
-          case 0x50: // Incoming data
-            _process_pod_data(sender, msg, len);
+          case 0x02:  // launch detected          //flight state 0x02-descent not yet implemented
+            flightState = 0x01;
+            break;
+          case 0x03:  // landing confirmed
+            flightState = 0x03;
+            break;
+          case 0x20: // set clock
+            _process_clock_set(msg);
+            break;
+          case 0x21: // test clock
+            _process_clock_test(msg);
+            break;
+          case 0x40: // request data
+            _process_request_data();
             break;
           default:
             // Nothing should fall into this category
@@ -31,4 +47,37 @@ void FC_to_CM::_listen_for_rx() {
       }
     }
   }
+}
+ 
+void FC_to_CM::_process_invitation() {    //sends 0x10 trasmission to confirm invitation, and responce (decline, clock only, clock and data)
+  char msg[2];
+  msg[0] = 0x10;
+  msg[1] = rsvpState;
+  _xbee.txAddressed(_cmAddress, msg, 2);
+}
+
+void FC_to_CM::_process_clock_set(char* timeBytes) {
+  uint32_t t = 0;
+  for (int i = 0; i < 4; i++){            //convert 4 bytes into time_t type
+    t |= timeBytes[i+1] << (8 * i);
+  }
+  set_time(time_t(t));                    //set mbed clock to UNIX time
+}
+
+void FC_to_CM::_process_clock_test(char* timeBytes) {
+  char msg[2];
+  msg[0] = 0x31;
+  uint32_t t;
+  for (int i = 0; i < 4; i++){            //convert 4 bytes into time_t type
+    t |= timeBytes[i+1] << (8 * i);
+  }
+  if (t != time(NULL)) {                  //compare local time to CM time
+    msg[1] = 0x00;  //fail
+  } else {
+    msg[1] = 0x01;  //pass
+  }
+  _xbee.txAddressed(_cmAddress, msg, 2);
+}
+void FC_to_CM::_process_request_data(){
+  //send some data  
 }
--- src/FC_to_CM.h
+++ src/FC_to_CM.h
@@ -1,25 +1,40 @@
 #ifndef FC_TO_CM_H
 #define FC_TO_CM_H
 
+#include "XBeeAPIParser.h"
+#include "mbed.h"
+#include "rtos.h"
+#include <string> 
+
+#define MAX_FC 6
+#define MAX_MSG_LENGTH 110
+#define MAX_POD_DATA_BYTES 100
+
 class FC_to_CM {
 private:
   XBeeAPIParser _xbee;
   uint32_t _timeout;
+  uint64_t _cmAddress;
+
+  //RTOS management
+  Thread _rx_thread;
 
   void _listen_for_rx();                //listens for rx, then runs one of the appropriate responces below, may handle smaller tasks like switching flags 
   void _process_invitation();           //if recieved 0x00, sends 0x10
-  void _process_clock_set();
-  void _process_test_clock();           //if recieved 0x21, send 0x31 
+  void _process_clock_set(char*);
+  void _process_clock_test(char*);           //if recieved 0x21, send 0x31 
   void _process_request_data();         //if recieved 0x40, send 0x50
 
-  bool primedForLaunch;
-  //int dataInterval;                     //in seconds
-  bool launchDetected;
-  bool landingConfirmed;
+  char flightState;                     //stores the state of the flight from CM broadcasts--enum: preLaunch(0x00),ascent(0x01),descent(0x02),landed(0x03),labTest(0x10)
+  char dataInterval;                    //in seconds
+  bool goodClock;
+
+  char rsvpState;
+
 
 public:
-
-
+  FC_to_CM(PinName tx, PinName rx);
+  
 };
 
 #endif
